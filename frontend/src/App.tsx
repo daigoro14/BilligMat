d03ff95f81f0511476b18d088861cefc301c5ea5
daigoro <daigoropersson@gmail.com>
2023-08-18 00:07:31 +0200

    submodule removed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+
+export default function HomePage() {
+
+    // useEffect(() => {
+    //     fetch(`https://www.willys.se/c/glass-godis-och-snacks?size=100&page=1&sort=topRated&avoidCache=1691925655012`, {
+    //         // mode: 'cors',
+    //     })
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data)
+    //     })
+    // }, [])
+
+    // async function fetchData() {
+    //     fetch(`https://www.willys.se/c/glass-godis-och-snacks?size=100&page=1&sort=topRated&avoidCache=1691925655012`, {
+    //         mode: 'cors',
+    //     })
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data)
+    //     })
+    // }
+
+  return (
+    <div>
+        <h1>Billig Mat</h1>
+    </div>
+  )
+}
